da5aeb7a13a6b932fa972ab2d375f83d1dccaca0
slhuang520 <349733050@qq.com>
2019-05-03 16:05:31 +0800

    update writyFileSync

--- web/nodeJS/base/fs.js
+++ web/nodeJS/base/fs.js
@@ -2,6 +2,6 @@ var fs = require("fs");
 
 console.log(11111111);
 //同步写文件
-var res = fs.writeFileSync("./txt/t1.txt");
+var res = fs.writeFileSync("./txt/t1.txt", "这是一行文字");
 console.log(res);
 console.log(22222222);
